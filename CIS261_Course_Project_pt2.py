507337550013d7944b316581801cb4e83ad1ab6a
username <useremail>
2023-11-19 14:18:21 -0800

    Part 3

--- CIS261_Course_Project_pt2.py
+++ CIS261_Course_Project_pt2.py
@@ -2,6 +2,10 @@
 #CIS261 
 #Course Project Phase 2
 
+from stat import FILE_ATTRIBUTE_COMPRESSED
+from wsgiref import validate
+
+
 def getDatesWorked():
     fromDate = input("Please enter start date in the following format MM/DD/YYYY: ")
     endDate = input("Please enter end date in the following fromat MM/DD/YYYY: ")
@@ -12,7 +16,7 @@ def getEmpName():
     return empName
 
 def getHoursWorked():
-    hours = float(input("Enter Hours: "))
+    hours = float(input("Enter amount of hours worked: "))
     return hours
 
 def getHourlyRate():
@@ -60,35 +64,82 @@ def printInfo(empDetailList):
         empTotals["totNet"] = totalNetPay
         
 def printTotals(empTotals):
+    print()
     print(f'Total Number Of Employees:  {empTotals["totEmp"]}')
     print(f'Total Hours Of Employee:   {empTotals["totHours"]}')
-    print(f'Total Gross Pay Of Employees:   {empTotals["totGross"]}')
-    print(f'Total Tax Of Employees:   {empTotals["totTax"]}')
-    print(f'Total Net Pay Of Employees:  {empTotals["totNet"]}')
+    print(f"Total Gross Pay Of Employees:   {empTotals['totGross']:,.2f}")
+    print(f"Total Tax Of Employees:   {empTotals['totTax']:,.2f}")
+    print(f"Total Net Pay Of Employees:  {empTotals['totNet']:,.2f}")
+    
+def WriteEmployeeInformation(employee):
+    file = open("employeeinfo.txt", "a")
+    
+    file.write('{}|{}|{}|{}|{}|{}\n'.format(employee[0], employee[1], employee[2], employee[3], employee[4], employee[5]))
+      
+def GetFromDate():
+     valid = False
+     fromdate = ""
+
+     while not valid:
+    
+        fromdate = input("Enter From Date (mm/dd/yyy): ")
+        if (len(fromdate.split('/')) != 3 and fromdate.upper() != 'ALL'):
+            print("Invalid Date Format: ")
+        else:
+            valid = True
+     return fromdate
+
+def ReadEmployeeInformation(fromdate):
+    EmpDetailList = []
+    
+    file = open("employeeinfo.txt", "r")
+    data = file.readlines()
+    
+    condition = True
+    if fromdate.upper() == 'ALL':
+        condition = False
         
+    for employee in data:
+    
+        employee = [x.strip() for x in  employee.strip().split("|")]
+        
+        if not condition:
+            EmpDetailList.append([employee[0], employee[1], employee[2], float(employee[3]),float(employee[4]), float(employee[5])])
+        else:
+            if fromdate ==employee[0]:
+                EmpDetailList.append([employee[0], employee[1], employee[2], float(employee[3]), float(employee[4]), float(employee[5])])
+     
+    return EmpDetailList
+
 if __name__=="__main__":
+    
     empDetailList = []
     empTotals = {}
+   
     while True:
         empName = getEmpName()
         if (empName.lower() == "end"):
             break
+       
         fromDate, endDate = getDatesWorked()
         hours = getHoursWorked()
         hourlyRate = getHourlyRate()
         taxRate = getTaxRate()
-        empDetail = []
-        empDetail.insert(0, fromDate)
-        empDetail.insert(1, endDate)
-        empDetail.insert(2, empName)
-        empDetail.insert(3, hours)
-        empDetail.insert(4, hourlyRate)
-        empDetail.insert(5, taxRate)
-        empDetailList.append(empDetail)
-    printInfo(empDetailList)
+       
+        print()
+        
+        EmpDetail = [fromDate, endDate, empName, hours, hourlyRate, taxRate]
+        WriteEmployeeInformation(EmpDetail)
+        
+    print()
+    print()
+    fromDate = GetFromDate()
+    
+    EmpDetailList = ReadEmployeeInformation(fromDate)
+    
+    print()
+    printInfo(EmpDetailList)
+    print()
     printTotals(empTotals)
-    
-    
-
-    
-
+        
+       
